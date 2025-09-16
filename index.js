3be13ca2cca869a4c6cb92a0e1d6626bd630c716
Martin Martin <morskijk@gmail.com>
2025-09-16 08:35:27 +0300

    added site css

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 import http from 'http';
 import { catsHtml } from './src/cats.js'
+import { css } from './src/siteCss.js';
 
 let server = http.createServer((req, res) => {
     if (req.url == "/") {
         res.writeHead(200, { "content-type": "text/html" });
         res.write(catsHtml);
+    }
+    else if (req.url == "/content/styles/site.css") {
+        res.writeHead(214, { "content-type": "text/css" })
+        res.write(css);
     }
     res.end();
 });
