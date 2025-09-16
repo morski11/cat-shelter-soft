aeaff4fd3381c51a7cd1a6b108cc7b9f7a26c753
Martin Martin <morskijk@gmail.com>
2025-09-16 09:07:34 +0300

    introduced fs promises

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 import http from 'http';
-import { catsHtml } from './src/cats.js'
-import { css } from './src/siteCss.js';
+import { cats } from './src/cats.js';
+import fs from 'fs/promises';
 
-let server = http.createServer((req, res) => {
+let server = http.createServer( async (req, res) => {
     if (req.url == "/") {
+        //const catsData = await fs.readFile('src/cats.js', {encoding: "utf-8"});
+        const catHtml = await fs.readFile(`views/home/index.html`);
         res.writeHead(200, { "content-type": "text/html" });
-        res.write(catsHtml);
+        res.write(catHtml);
     }
     else if (req.url == "/content/styles/site.css") {
+        const siteCss = await fs.readFile('content/styles/site.css'); 
         res.writeHead(214, { "content-type": "text/css" })
-        res.write(css);
+        res.write(siteCss);
     }
     res.end();
 });
--- src/cats.js
+++ src/cats.js
@@ -1,89 +1,39 @@
-export const catsHtml = `<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="https://use.fontawesome.com/releases/v5.0.7/css/all.css" rel="stylesheet">
-    <link rel="stylesheet" href="../../content/styles/site.css">
-    <link rel="shortcut icon" href="https://img.icons8.com/ios-filled/50/000000/cat-footprint.png" type="image/png">
-    <title>Cat Shelter</title>
-</head>
-
-<body>
-    <header>
-        <nav>
-            <ul class="navigation">
-                <li><a href="/">Home Page</a></li>
-                <li><a href="/cats/add-breed">Add Breed</a></li>
-                <li><a href="/cats/add-cat">Add Cat</a></li>
-            </ul>
-        </nav>
-        <h1>Cat Shelter</h1>
-        <form action="/search">
-            <input type="text">
-            <button type="button">Search</button>
-        </form>
-    </header>
-
-    <main>
-        <section class="cats">
-            <ul>
-                <li>
-                    <img src="https://ichef.bbci.co.uk/news/976/cpsprodpb/12A9B/production/_111434467_gettyimages-1143489763.jpg" alt="Black Cat">
-                    <h3></h3>
-                    <p><span>Breed: </span>Bombay Cat</p>
-                    <p><span>Description: </span>Dominant and aggressive to other cats. Will probably eat you in your sleep. Very cute tho.</p>
-                    <ul class="buttons">
-                        <li class="btn edit"><a href="">Change Info</a></li>
-                        <li class="btn delete"><a href="">New Home</a></li>
-                    </ul>
-                </li>
-                <li>
-                    <img src="https://cdn.pixabay.com/photo/2015/06/19/14/20/cat-814952_1280.jpg" alt="">
-                    <h3>Pretty Kitty</h3>
-                    <p><span>Breed: </span>Bombay Cat</p>
-                    <p><span>Description: </span>Dominant and aggressive to other cats. Will probably eat you in your sleep. Very cute tho.</p>
-                    <ul class="buttons">
-                        <li class="btn edit"><a href="">Change Info</a></li>
-                        <li class="btn delete"><a href="">New Home</a></li>
-                    </ul>
-                </li>
-                <li>
-                    <img src="https://cdn.pixabay.com/photo/2018/08/08/05/12/cat-3591348_1280.jpg" alt="Black Cat">
-                    <h3>Pretty Kitty</h3>
-                    <p><span>Breed: </span>Bombay Cat</p>
-                    <p><span>Description: </span>Dominant and aggressive to other cats. Will probably eat you in your sleep. Very cute tho.</p>
-                    <ul class="buttons">
-                        <li class="btn edit"><a href="">Change Info</a></li>
-                        <li class="btn delete"><a href="">New Home</a></li>
-                    </ul>
-                </li>
-                <li>
-                    <img src="https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg" alt="Black Cat">
-                    <h3>Pretty Kitty</h3>
-                    <p><span>Price: </span>350$</p>
-                    <p><span>Breed: </span>Bombay Cat</p>
-                    <p><span>Description: </span>Dominant and aggressive to other cats. Will probably eat you in your sleep. Very cute tho.</p>
-                    <ul class="buttons">
-                        <li class="btn edit"><a href="">Change Info</a></li>
-                        <li class="btn delete"><a href="">New Home</a></li>
-                    </ul>
-                </li>
-                <li>
-                    <img src="https://cdn.pixabay.com/photo/2014/04/13/20/49/cat-323262_1280.jpg" alt="Black Cat">
-                    <h3>Pretty Kitty</h3>
-                    <p><span>Breed: </span>Bombay Cat</p>
-                    <p><span>Description: </span>Dominant and aggressive to other cats. Will probably eat you in your sleep. Very cute tho.</p>
-                    <ul class="buttons">
-                        <li class="btn edit"><a href="">Change Info</a></li>
-                        <li class="btn delete"><a href="">New Home</a></li>
-                    </ul>
-                </li>
-            </ul>
-        </section>
-    </main>
-
-</body>
-</html>`;
+export const cats = {
+    "cats": [
+        {
+            "id": "1",
+            "name": "Luna",
+            "breed": "Siamese",
+            "imageUrl": "https://ichef.bbci.co.uk/news/976/cpsprodpb/12A9B/production/_111434467_gettyimages-1143489763.jpg",
+            "description": "Luna is a playful and curious Siamese cat who loves chasing laser pointers and cuddling in warm blankets."
+        },
+        {
+            "id": "2",
+            "name": "Oliver",
+            "breed": "Maine Coon",
+            "imageUrl": "https://cdn.pixabay.com/photo/2015/06/19/14/20/cat-814952_1280.jpg",
+            "description": "Oliver is a gentle giant with a fluffy coat. He enjoys climbing cat trees and watching birds out the window."
+        },
+        {
+            "id": "3",
+            "name": "Milo",
+            "breed": "British Shorthair",
+            "imageUrl": "https://cdn.pixabay.com/photo/2018/08/08/05/12/cat-3591348_1280.jpg",
+            "description": "Milo is calm and affectionate, often found lounging in sunbeams or purring on the couch."
+        },
+        {
+            "id": "4",
+            "name": "Cleo",
+            "breed": "Bengal",
+            "imageUrl": "https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg",
+            "description": "Cleo is an adventurous Bengal with stunning spots. She’s always exploring new corners of the house."
+        },
+        {
+            "id": "5",
+            "name": "Willow",
+            "breed": "Ragdoll",
+            "imageUrl": "https://cdn.pixabay.com/photo/2014/04/13/20/49/cat-323262_1280.jpg",
+            "description": "Willow is a sweet and laid-back Ragdoll who loves being held and follows her humans around everywhere."
+        }
+    ]
+}
