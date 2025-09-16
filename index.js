e35473cdacf96cbf2341efad121d38a07922d8f7
Martin Martin <morskijk@gmail.com>
2025-09-16 09:27:22 +0300

    add breed view added

--- index.js
+++ index.js
@@ -2,20 +2,36 @@ import http from 'http';
 import { cats } from './src/cats.js';
 import fs from 'fs/promises';
 
-let server = http.createServer( async (req, res) => {
+let server = http.createServer(async (req, res) => {
     if (req.url == "/") {
         //const catsData = await fs.readFile('src/cats.js', {encoding: "utf-8"});
-        const catHtml = await fs.readFile(`views/home/index.html`);
+        const homeHtml = await homeView();
         res.writeHead(200, { "content-type": "text/html" });
-        res.write(catHtml);
+        res.write(homeHtml);
     }
     else if (req.url == "/content/styles/site.css") {
-        const siteCss = await fs.readFile('content/styles/site.css'); 
+        const siteCss = await fs.readFile('content/styles/site.css');
         res.writeHead(214, { "content-type": "text/css" })
         res.write(siteCss);
     }
+    else if (req.url == "/cats/add-breed") {
+        const addBreedHtml = await addBreedView();
+        res.writeHead(200, { "content-type": "text/html" });
+        res.write(addBreedHtml);
+    }
     res.end();
 });
+
+
+async function homeView() {
+    const html = await fs.readFile(`views/home/index.html`, { encoding: "utf-8" });
+    return html;
+}
+
+async function addBreedView() {
+    const html = await fs.readFile(`views/addBreed.html`, { encoding: "utf-8" });
+    return html;
+}
 
 
 server.listen(5000);
